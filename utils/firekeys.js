a5fe5be0809ce6d42d59e3b18cbc0fcf57065c4d
Andy Alcantara <andyalcantara745@yahoo.com>
2019-05-01 11:26:07 -0400

    Delete firekeys.js

--- utils/firekeys.js
+++
@@ -1,8 +0,0 @@
-export const firekeys = {
-    apiKey: "AIzaSyDtgsFRPUsaYkLKWPfOyOH-y5xSt9gi6ZQ",
-    authDomain: "gestor-a4baa.firebaseapp.com",
-    databaseURL: "https://gestor-a4baa.firebaseio.com",
-    projectId: "gestor-a4baa",
-    storageBucket: "gestor-a4baa.appspot.com",
-    messagingSenderId: "503288975364"
-};
