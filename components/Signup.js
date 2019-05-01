50236e99ed4bb9e829873ec58c76fd86f9593219
Andy Alcantara <andyalcantara745@yahoo.com>
2019-05-01 09:49:16 -0400

    Committing changes

--- components/Signup.js
+++ components/Signup.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
 import * as firebase from 'firebase';
+import firekeys from '../utils/firekeys';
 
 class Signup extends React.Component {
+
+    componentDidMount() {
+        firebase.initializeApp(firekeys);
+    }
+
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>Sign Up Component</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default Signup;
