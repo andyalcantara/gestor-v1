044f48eddc7e91bd953dab0b3ff8d87bd1b61f9b
Andy Alcantara <andyalcantara745@yahoo.com>
2019-05-20 14:42:57 -0400

    Add Section list on Total component

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,33 +1,55 @@
 import React from 'react';
-import { View, Text, FlatList, SafeAreaView } from "react-native";
+import { View, Text, FlatList, SafeAreaView, SectionList } from "react-native";
 
 import { connect } from 'react-redux';
 
 class Calculator extends React.Component {
+
     render() {
 
-        const { invoices } = this.props;
+        const { invoices, clinics, sectionListData } = this.props;
+        console.log(invoices, clinics, sectionListData);
 
         return (
             <SafeAreaView>
                 <Text>Here you can find the whole Income of your clinics</Text>
 
-                <FlatList
-                    data={invoices}
-                    renderItem={({item}) => (
-                        <View>
-                            <Text>{item.price}</Text>
-                        </View>
+                <SectionList
+                    renderItem={({item, index, section}) => (
+                        <Text>{item.price}</Text>
                     )}
+                    renderSectionHeader={({section: {name}}) => (
+                        <Text>{name}</Text>
+                    )}
+                    sections={sectionListData}
                 />
+
             </SafeAreaView>
         );
     }
 }
 
-function mapStateToProps({ invoices }) {
+function mapStateToProps({ invoices, clinics }) {
+
+    let invoicesArray = Object.keys(invoices).map(key => invoices[key]);
+
+    let acClinics = Object.keys(clinics).map(key => clinics[key])
+        .map(clinic => {
+            let acInvoices = invoicesArray.filter(invoice => {
+               return invoice.clinic === clinic._id;
+            });
+            return {
+                name: clinic.name,
+                data: acInvoices
+            }
+        });
+
+    console.log(acClinics, 'These are actual clinics');
+
     return {
-        invoices: Object.keys(invoices).map(key => invoices[key])
+        invoices: Object.keys(invoices).map(key => invoices[key]),
+        clinics: Object.keys(clinics).map(key => clinics[key]),
+        sectionListData: acClinics,
     }
 }
 
