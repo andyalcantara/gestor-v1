0ad180ff251e00d71dd605857a688ddb43ee1170
Andy Alcantara <andyalcantara745@yahoo.com>
2019-05-20 12:17:28 -0400

    Add squeleton for total component

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { View, Text, FlatList, SafeAreaView } from "react-native";
 
+import { connect } from 'react-redux';
+
 class Calculator extends React.Component {
     render() {
+
+        const { invoices } = this.props;
+
         return (
             <SafeAreaView>
-                <Text>Calculator Component</Text>
+                <Text>Here you can find the whole Income of your clinics</Text>
+
+                <FlatList
+                    data={invoices}
+                    renderItem={({item}) => (
+                        <View>
+                            <Text>{item.price}</Text>
+                        </View>
+                    )}
+                />
             </SafeAreaView>
         );
     }
 }
 
-export default Calculator;
+function mapStateToProps({ invoices }) {
+    return {
+        invoices: Object.keys(invoices).map(key => invoices[key])
+    }
+}
+
+export default connect(mapStateToProps)(Calculator);
